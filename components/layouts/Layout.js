dceee4f6c8bc94588ea6da996df73a4733e7a2a5
mmc1999 <mcentorbi161999@gmail.com>
2022-04-11 14:43:21 -0300

    ss

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import React from 'react'
 import { Navbar } from '../ui';
 
+const origin = (typeof window === "undefined") ? "" : window.location.origin; 
+
 export const Layout = ({children, title = "Pokemon app"}) => {
   return (
     <>
